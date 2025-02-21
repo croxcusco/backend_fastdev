f6fa787503604d0da55e8b5d0442316159939a6f
hema san <hemasanjuan@gmail.com>
2025-02-21 17:55:31 -0500

    update cors

--- src/server.ts
+++ src/server.ts
@@ -16,8 +16,16 @@ const prisma01 = new PrismaClient();
 async function startApolloServer() {
     const app = express();
 
+    const allowedOrigins = ['https://crox.up.railway.app', 'http://localhost:5173']; // Agrega más orígenes aquí
+
     app.use(cors({
-        origin: 'https://crox.up.railway.app/', // Reemplaza con la URL de tu frontend
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+                callback(null, true);
+            } else {
+                callback(new Error('No permitido por CORS'));
+            }
+        },
         credentials: true,
     }));
     app.use(express.json());
